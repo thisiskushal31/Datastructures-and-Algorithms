cc61d7058bf6f1ecd40efe1e2e104ab017d5f0e2
thisiskushal31 <guptakushal131@gmail.com>
2021-12-30 00:20:35 +0530

    more Array question

---
+++ 01-Array-Questions/005-move-negative-numbers-beginning-positive-end-constant-extra-space.java
@@ -0,0 +1,40 @@
+// https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/
+
+import java.io.*;
+
+class GFG {
+
+	static void rearrange(int arr[], int n)
+	{
+		int j = 0, temp;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] < 0) {
+				if (i != j) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+				j++;
+			}
+		}
+	}
+
+	// A utility function to print an array
+	static void printArray(int arr[], int n)
+	{
+		for (int i = 0; i < n; i++)
+			System.out.print(arr[i] + " ");
+	}
+
+	// Driver code
+	public static void main(String args[])
+	{
+		int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+		int n = arr.length;
+
+		rearrange(arr, n);
+		printArray(arr, n);
+	}
+}
+
+// This code is contributed by Nikita Tiwari.
