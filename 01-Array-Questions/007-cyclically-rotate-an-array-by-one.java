3a5d4f680c7a74e967960248fb12123ce7f1bccb
thisiskushal31 <guptakushal131@gmail.com>
2021-12-31 01:55:09 +0530

    new questions added

---
+++ 01-Array-Questions/007-cyclically-rotate-an-array-by-one.java
@@ -0,0 +1,28 @@
+//Practice : https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
+
+//Editorial : https://www.geeksforgeeks.org/c-program-cyclically-rotate-array-one/
+
+import java.util.Arrays;
+
+public class Test {
+    static int arr[] = new int[] { 1, 2, 3, 4, 5 };
+
+    // Method for rotation
+    static void rotate() {
+        int x = arr[arr.length - 1], i;
+        for (i = arr.length - 1; i > 0; i--)
+            arr[i] = arr[i - 1];
+        arr[0] = x;
+    }
+
+    // Driver program
+    public static void main(String[] args) {
+        System.out.println("Given Array is");
+        System.out.println(Arrays.toString(arr));
+
+        rotate();
+
+        System.out.println("Rotated Array is");
+        System.out.println(Arrays.toString(arr));
+    }
+}
