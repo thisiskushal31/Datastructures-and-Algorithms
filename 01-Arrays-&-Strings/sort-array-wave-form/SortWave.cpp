b77b2befe4ef40b36c60baac00672b1e8f83aa0c
thisiskushal31 <guptakushal131@gmail.com>
2022-01-09 21:42:38 +0530

    Array Question

--- 01-Arrays-&-Strings/sort-array-wave-form/SortWave.cpp
+++ 01-Arrays-&-Strings/sort-array-wave-form/SortWave.cpp
@@ -23,9 +23,9 @@ class Solution{
                arr[i+1]=temp;
            }
        }
-       
-       for (int i = 0; i + 1 < n; i += 2)
-            swap(arr[i], arr[i + 1]);
+    //    Another apprroach
+    //    for (int i = 0; i + 1 < n; i += 2)
+    //         swap(arr[i], arr[i + 1]);
     }
 };
 
