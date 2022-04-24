0a19bd1ee36d17e364239588411dd7c6df411de4
thisiskushal31 <guptakushal131@gmail.com>
2022-04-24 20:48:04 +0530

    Merge Sorted Array Added

---
+++ Leetcode/Two_Pointers/088.Merge-Sorted-Array/88.Merge-Sorted-Array.java
@@ -0,0 +1,17 @@
+//Approach #2: Two Pointers
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
+        while (tail1 >= 0 && tail2 >= 0) {
+            nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? nums1[tail1--] : nums2[tail2--];
+        }
+
+        while (tail2 >= 0) { //only need to combine with remaining nums2, if any
+            nums1[finished--] = nums2[tail2--];
+        }
+    }
+}
+
+// Time complexity = O(k), k = m + n
+// Space complexity = O(1)
