f53cc6b1fefb0f1741d18b26d4d5cf25da93c426
thisiskushal31 <guptakushal131@gmail.com>
2022-04-29 23:26:03 +0530

    Added 141. Linked List Cycle

--- Leetcode/Two_Pointers/141.Linked-List-Cycle/141-Linked-List-Cycle.cpp
+++ Leetcode/Two_Pointers/141.Linked-List-Cycle/141-Linked-List-Cycle.cpp
@@ -1,1 +1,35 @@
-// To be added
+// Solution 1: Floyd's Cycle-Finding Algorithm 
+// Fast and Slow Pointer
+
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL)
+            return false;
+        
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow)
+                return true;
+        }
+        
+        return false;
+    }
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
