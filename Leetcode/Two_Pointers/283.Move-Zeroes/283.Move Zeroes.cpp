1539f3ca9137a7a4243977c061649fd72f5d5b05
thisiskushal31 <guptakushal131@gmail.com>
2022-05-05 23:29:51 +0530

    Added 283.Move-Zeroes

--- Leetcode/Two_Pointers/283.Move-Zeroes/283.Move Zeroes.cpp
+++ Leetcode/Two_Pointers/283.Move-Zeroes/283.Move Zeroes.cpp
@@ -1,1 +1,24 @@
-// To be added
+// Solution 2: Simple O(N) C++ Solution
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int left=0;
+        int right=0;
+        
+        while (right<nums.size())
+        {
+            if (nums[right]==0)
+            {
+                right++;
+            }
+            else
+            {
+                swap(nums[left],nums[right]);
+                left++;
+                right++;
+            }
+        }
+    }
+};
