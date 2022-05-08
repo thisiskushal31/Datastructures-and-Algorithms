ba0e7cebde13330063d2b02f69b1bd068cea8513
thisiskushal31 <guptakushal131@gmail.com>
2022-05-08 23:43:16 +0530

    Added 713.Subarray-Product-Less-Than-K

--- Leetcode/Two_Pointers/713.Subarray-Product-Less-Than-K/713.Subarray-Product-Less-Than-K.cpp
+++ Leetcode/Two_Pointers/713.Subarray-Product-Less-Than-K/713.Subarray-Product-Less-Than-K.cpp
@@ -1,1 +1,48 @@
-// To be added
+// Solution 1: O(n) time, O(1) space O(n) time, O(1) space (sliding window) -- C++ solution
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) 
+    {
+        if (k<=1) return 0;
+        int j=0;
+        int prod = 1;
+        int count = 0;
+        for (int i=0; i<nums.size(); i++)
+        {            
+            if (j<i)
+            {
+                j = i;
+                prod = 1;
+            }
+            
+            while (j<nums.size() && prod*nums[j]<k)
+            {
+                prod = prod*nums[j];
+                j++;
+            }
+            count += j-i;
+            prod = prod/nums[i];
+        }
+        return count;
+    }
+};
+
+// Solution 2: O(n) time, O(1) space O(n) time, O(1) space (sliding window) -- C++ solution
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k == 0) return 0;
+        int cnt = 0;
+        int pro = 1;
+        for (int i = 0, j = 0; j < nums.size(); j++) {
+            pro *= nums[j];
+            while (i <= j && pro >= k) {
+                pro /= nums[i++];
+            }
+            cnt += j - i + 1;
+        }
+        return cnt;
+    }
+};
---
+++ Leetcode/Two_Pointers/713.Subarray-Product-Less-Than-K/713.Subarray-Product-Less-Than-K.java
@@ -0,0 +1,17 @@
+// Solution 3: O(n) time, O(1) space (sliding window) -- Java solution
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k == 0) return 0;
+        int cnt = 0;
+        int pro = 1;
+        for (int i = 0, j = 0; j < nums.length; j++) {
+            pro *= nums[j];
+            while (i <= j && pro >= k) {
+                pro /= nums[i++];
+            }
+            cnt += j - i + 1;
+        }
+        return cnt;
+    }
+}
